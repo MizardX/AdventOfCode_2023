80173f7f628abc1974ca198aa7a48dff7bfed08f
Markus Jarderot <mizardx@gmail.com>
2023-12-03 14:24:51 +0100

    Day 1 - Clippy fixes

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,3 +1,5 @@
+#![warn(clippy::pedantic)]
+
 pub fn run() {
     println!(".Day 01");
 
@@ -85,19 +87,18 @@ fn part_2(input: &[Input]) -> usize {
         for &ch in line.as_bytes() {
             let (dig, state1) = match (state, ch) {
                 (_, b @ b'0'..=b'9') => (Some(b - b'0'), State::Start),
-                (State::O, b'n') => (None, State::On),
+                (State::O | State::Fo, b'n') => (None, State::On),
                 (State::On, b'e') => (Some(1), State::E),
-                (State::On, b'i') => (None, State::Ni),
+                (State::On | State::N | State::Nin, b'i') => (None, State::Ni),
                 (State::T, b'w') => (None, State::Tw),
                 (State::Tw, b'o') => (Some(2), State::O),
                 (State::T, b'h') => (None, State::Th),
                 (State::Th, b'r') => (None, State::Thr),
                 (State::Thr, b'e') => (None, State::Thre),
                 (State::Thre, b'e') => (Some(3), State::E),
-                (State::Thre, b'i') => (None, State::Ei),
+                (State::Thre | State::Se | State::Seve | State::E, b'i') => (None, State::Ei),
                 (State::F, b'o') => (None, State::Fo),
                 (State::Fo, b'u') => (None, State::Fou),
-                (State::Fo, b'n') => (None, State::On),
                 (State::Fou, b'r') => (Some(4), State::Start),
                 (State::F, b'i') => (None, State::Fi),
                 (State::Fi, b'v') => (None, State::Fiv),
@@ -106,24 +107,19 @@ fn part_2(input: &[Input]) -> usize {
                 (State::Si, b'x') => (Some(6), State::Start),
                 (State::S, b'e') => (None, State::Se),
                 (State::Se, b'v') => (None, State::Sev),
-                (State::Se, b'i') => (None, State::Ei),
                 (State::Sev, b'e') => (None, State::Seve),
                 (State::Seve, b'n') => (Some(7), State::N),
-                (State::Seve, b'i') => (None, State::Ei),
-                (State::E, b'i') => (None, State::Ei),
                 (State::Ei, b'g') => (None, State::Eig),
                 (State::Eig, b'h') => (None, State::Eigh),
                 (State::Eigh, b't') => (Some(8), State::T),
-                (State::N, b'i') => (None, State::Ni),
                 (State::Ni, b'n') => (None, State::Nin),
                 (State::Nin, b'e') => (Some(9), State::E),
-                (State::Nin, b'i') => (None, State::Ni),
                 (_, b'o') => (None, State::O),
                 (_, b't') => (None, State::T),
                 (_, b'f') => (None, State::F),
                 (_, b's') => (None, State::S),
                 (_, b'e') => (None, State::E),
-                (_, b'n') => (None, State::N),
+                (_, b'n') => (None, State::N),                
                 (_, _) => (None, State::Start),
             };
             // println!("  {state:?} + {} -> {dig:?} + {state1:?}", ch as char);
