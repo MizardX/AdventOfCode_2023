72b33b01e48040d1a92cdadcfd9b8462bb0d537e
Markus Jarderot <mizardx@gmail.com>
2023-12-13 22:25:47 +0100

    Update template

--- src/dayXX/mod.rs
+++ src/dayXX/mod.rs
@@ -1,5 +1,8 @@
 #![warn(clippy::pedantic)]
 
+use std::str::FromStr;
+use thiserror::Error;
+
 const EXAMPLE: &str = include_str!("example.txt");
 const INPUT: &str = include_str!("input.txt");
 
@@ -7,38 +10,43 @@ pub fn run() {
     println!(".Day XX");
 
     println!("++Example");
-    let example = parse_input(EXAMPLE);
+    let example = EXAMPLE.parse().expect("Parse example");
     println!("|+-Part 1: {} (expected XXX)", part_1(&example));
     println!("|'-Part 2: {} (expected XXX)", part_2(&example));
 
     println!("++Input");
-    let input = parse_input(INPUT);
+    let input = INPUT.parse().expect("Parse input");
     println!("|+-Part 1: {} (expected XXX)", part_1(&input));
     println!("|'-Part 2: {} (expected XXX)", part_2(&input));
     println!("')");
 }
 
-#[allow(unused)]
-fn part_1(input: &[Input]) -> usize {
+fn part_1(_input: &Input) -> usize {
     0
 }
 
-#[allow(unused)]
-fn part_2(input: &[Input]) -> usize {
+fn part_2(_input: &Input) -> usize {
     0
 }
 
 #[derive(Debug, Clone)]
-struct Input(i32);
+struct Input {}
 
-fn parse_input(text: &str) -> Vec<Input> {
-    let mut res: Vec<Input> = Vec::new();
-    for line in text.lines() {
-        res.push(Input(line.trim().parse::<i32>().unwrap()));
-    }
-    res
+#[derive(Debug, Error)]
+enum ParseInputError {
+    // #[error("Input is empty")]
+    // EmptyInput,
+    // #[error("Unexpected character: '{0}'")]
+    // InvalidChar(char),
 }
 
+impl FromStr for Input {
+    type Err = ParseInputError;
+
+    fn from_str(_text: &str) -> Result<Self, Self::Err> {
+        todo!()
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -47,21 +55,20 @@ mod tests {
     use super::*;
     use test::Bencher;
 
-
     #[bench]
     fn run_parse_input(b: &mut Bencher) {
-        b.iter(|| black_box(parse_input(INPUT)));
+        b.iter(|| black_box(INPUT.parse::<Input>().expect("Parse input")));
     }
 
     #[bench]
     fn run_part_1(b: &mut Bencher) {
-        let input = parse_input(INPUT);
+        let input = INPUT.parse().expect("Parse input");
         b.iter(|| black_box(part_1(&input)));
     }
 
     #[bench]
     fn run_part_2(b: &mut Bencher) {
-        let input = parse_input(INPUT);
+        let input = INPUT.parse().expect("Parse input");
         b.iter(|| black_box(part_2(&input)));
     }
 }
