ffd65a5473643df87fe96881ef07baba38bb01c9
Markus Jarderot <mizardx@gmail.com>
2023-12-20 23:48:43 +0100

    Day 12 - Refactor

--- src/day20/mod.rs
+++ src/day20/mod.rs
@@ -14,184 +14,148 @@ pub fn run() {
     println!(".Day 20");
 
     println!("++Example 1");
-    let example = Input::try_from(EXAMPLE1).expect("Parse example 1");
+    let example = Circuit::try_from(EXAMPLE1).expect("Parse example 1");
     println!("|+-Part 1: {} (expected 32000000)", part_1(&example));
 
     println!("++Example 2");
-    let example = Input::try_from(EXAMPLE2).expect("Parse example 2");
+    let example = Circuit::try_from(EXAMPLE2).expect("Parse example 2");
     println!("|+-Part 1: {} (expected 11687500)", part_1(&example));
 
     println!("++Input");
-    let input = Input::try_from(INPUT).expect("Parse input");
+    let input = Circuit::try_from(INPUT).expect("Parse input");
     println!("|+-Part 1: {} (expected 899848294)", part_1(&input));
     println!("|'-Part 2: {} (expected 247454898168563)", part_2(&input));
     println!("')");
 }
 
-fn part_1(input: &Input) -> usize {
-    let mut low_count = 0;
-    let mut high_count = 0;
-    button_mash(input, &mut |button_press, _, _, signal| {
-        if button_press == 1001 {
-            return Some(low_count * high_count);
-        }
-
-        if signal {
-            high_count += 1;
-        } else {
-            low_count += 1;
-        }
-
-        None
-    })
+fn part_1(input: &Circuit) -> usize {
+    let mut simulator = CircuitSimulator::new(input);
+    for _ in 0..1000 {
+        simulator.press_button_once();
+    }
+    simulator.low_count * simulator.high_count
 }
 
-fn part_2(input: &Input) -> usize {
-    let rx_parent = try { input.modules[input.rx_index?].sources[0] };
-    let mut rx_parent_cycle = [None; 4];
-    button_mash(input, &mut |button_press, source, activation, signal| {
-        if Some(activation) == rx_parent && signal {
-            let source_index = input.modules[activation]
+fn part_2(input: &Circuit) -> usize {
+    let mut simulator = CircuitSimulator::new(input);
+    loop {
+        simulator.press_button_once();
+        if simulator.cycle_counts.iter().all(Option::is_some) {
+            return simulator
+                .cycle_counts
+                .iter()
+                .flatten()
+                .copied()
+                .reduce(lcm)
+                .unwrap();
+        }
+    }
+}
+
+#[derive(Debug)]
+struct CircuitSimulator<'a> {
+    circuit: &'a Circuit<'a>,
+    state: u64,
+    pending: VecDeque<(usize, usize, bool)>,
+    low_count: usize,
+    high_count: usize,
+    cycle_counts: SmallVec<[Option<usize>; 4]>,
+    button_presses: usize,
+}
+
+impl<'a> CircuitSimulator<'a> {
+    fn new(circuit: &'a Circuit<'a>) -> Self {
+        Self {
+            circuit,
+            state: 0,
+            pending: VecDeque::with_capacity(50),
+            low_count: 0,
+            high_count: 0,
+            cycle_counts: smallvec![None; 4],
+            button_presses: 0,
+        }
+    }
+
+    fn press_button_once(&mut self) {
+        self.button_presses += 1;
+        self.pending.push_back((
+            self.circuit.button_index,
+            self.circuit.broadcast_index,
+            false,
+        ));
+        while self.propagate_one_signal() {}
+    }
+
+    fn propagate_one_signal(&mut self) -> bool {
+        let Some((source, destination, is_high)) = self.pending.pop_front() else {
+            return false;
+        };
+        let module = &self.circuit.modules[destination];
+        let sent_signal = match (module.subtype, is_high) {
+            (GateType::Button, _) => Some(false),
+            (GateType::Identity, is_high) => Some(is_high),
+            (GateType::FlipFlop, true) => None,
+            (GateType::FlipFlop, false) => {
+                let bit = 1u64 << destination;
+                self.state ^= bit;
+                Some((self.state & bit) != 0)
+            }
+            (GateType::Conjunction, true) => {
+                let bit = 1u64 << source;
+                self.state |= bit;
+                Some((self.state & module.source_mask) != module.source_mask)
+            }
+            (GateType::Conjunction, false) => {
+                let bit = 1u64 << source;
+                self.state &= !bit;
+                Some((self.state & module.source_mask) != module.source_mask)
+            }
+        };
+
+        if let Some(new_signal) = sent_signal {
+            for &dest in &module.destinations {
+                self.pending.push_back((destination, dest, new_signal));
+            }
+        }
+
+        if is_high {
+            self.high_count += 1;
+        } else {
+            self.low_count += 1;
+        }
+
+        if is_high && Some(destination) == self.circuit.rx_source_index {
+            let source_index = self.circuit.modules[destination]
                 .sources
                 .iter()
                 .position(|&i| i == source)
                 .unwrap();
-            rx_parent_cycle[source_index] = Some(button_press);
-            if rx_parent_cycle.iter().all(Option::is_some) {
-                return Some(rx_parent_cycle.into_iter().flatten().reduce(lcm).unwrap());
-            }
+            self.cycle_counts[source_index] = Some(self.button_presses);
         }
-        None
-    })
-}
 
-fn button_mash(
-    input: &Input,
-    body: &mut impl FnMut(usize, usize, usize, bool) -> Option<usize>,
-) -> usize {
-    let mut state: u64 = 0;
-    let mut pending = VecDeque::new();
-    let broadcast_index = input.modules[input.button_index].destinations[0];
-    for button_press in 1.. {
-        pending.push_back((input.button_index, broadcast_index, false));
-        while let Some((source, activation, signal)) = pending.pop_front() {
-            let module = &input.modules[activation];
-            let sent_signal = match module.subtype {
-                ModuleType::Button => Some(false),
-                ModuleType::Broadcaster => Some(signal),
-                ModuleType::Sink => None,
-                ModuleType::FlipFlop => {
-                    if signal {
-                        None
-                    } else {
-                        let bit = 1u64 << activation;
-                        state ^= bit;
-                        Some((state & bit) != 0)
-                    }
-                }
-                ModuleType::Conjunction => {
-                    let bit = 1u64 << source;
-                    state = if signal { state | bit } else { state & !bit };
-                    Some((state & module.source_mask) != module.source_mask)
-                }
-            };
-
-            if let Some(res) = body(button_press, source, activation, signal) {
-                return res;
-            }
-            if let Some(new_signal) = sent_signal {
-                for &dest in &module.destinations {
-                    pending.push_back((activation, dest, new_signal));
-                }
-            }
-        }
+        true
     }
-    unreachable!()
 }
 
 #[derive(Debug, Clone, Copy)]
-enum ModuleType {
+enum GateType {
     Button,
-    Broadcaster,
-    Sink,
+    Identity,
     FlipFlop,
     Conjunction,
 }
 
-struct ModuleBuilder<'a> {
-    name: &'a str,
-    module_type: ModuleType,
-    destinations: SmallVec<[&'a str; 6]>,
-}
-
-impl<'a> ModuleBuilder<'a> {
-    pub fn new(
-        name: &'a str,
-        module_type: ModuleType,
-        destinations: SmallVec<[&'a str; 6]>,
-    ) -> Self {
-        Self {
-            name,
-            module_type,
-            destinations,
-        }
-    }
-
-    pub fn build(
-        self,
-        name_lookup: &HashMap<&'a str, usize>,
-    ) -> Result<Module<'a>, ParseInputError<'a>> {
-        let destinations = self
-            .destinations
-            .iter()
-            .map(|n| Ok(*name_lookup.get(n).ok_or(ParseInputError::InvalidName(n))?))
-            .collect::<Result<_, _>>()?;
-
-        Ok(Module::new(self.name, self.module_type, destinations))
-    }
-}
-
-impl<'a> TryFrom<&'a str> for ModuleBuilder<'a> {
-    type Error = ParseInputError<'a>;
-
-    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
-        if value.is_empty() {
-            return Err(ParseInputError::EmptyInput);
-        }
-        let (mut module_type, rest) = if let Some(rest) = value.strip_prefix('%') {
-            (ModuleType::FlipFlop, rest)
-        } else if let Some(rest) = value.strip_prefix('&') {
-            (ModuleType::Conjunction, rest)
-        } else {
-            (ModuleType::Broadcaster, value)
-        };
-        let (name, rest) = rest
-            .split_once(" -> ")
-            .ok_or(ParseInputError::ExpectedArrow)?;
-        let mut destinations = SmallVec::new();
-        for piece in rest.split(", ") {
-            destinations.push(piece);
-        }
-
-        if matches!(module_type, ModuleType::Broadcaster) && name != "broadcaster" {
-            module_type = ModuleType::Sink;
-        }
-
-        Ok(Self::new(name, module_type, destinations))
-    }
-}
-
 #[derive(Debug, Clone)]
-struct Module<'a> {
+struct Gate<'a> {
     _name: &'a str,
-    subtype: ModuleType,
+    subtype: GateType,
     destinations: SmallVec<[usize; 6]>,
     sources: SmallVec<[usize; 10]>,
     source_mask: u64,
 }
 
-impl<'a> Module<'a> {
-    fn new(name: &'a str, subtype: ModuleType, destinations: SmallVec<[usize; 6]>) -> Self {
+impl<'a> Gate<'a> {
+    fn new(name: &'a str, subtype: GateType, destinations: SmallVec<[usize; 6]>) -> Self {
         Self {
             _name: name,
             subtype,
@@ -199,6 +163,91 @@ impl<'a> Module<'a> {
             sources: SmallVec::new(),
             source_mask: 0,
         }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Circuit<'a> {
+    modules: Vec<Gate<'a>>,
+    button_index: usize,
+    broadcast_index: usize,
+    rx_source_index: Option<usize>,
+}
+
+impl<'a> Circuit<'a> {
+    fn new(
+        modules: Vec<Gate<'a>>,
+        button_index: usize,
+        broadcast_index: usize,
+        rx_source_index: Option<usize>,
+    ) -> Self {
+        Self {
+            modules,
+            button_index,
+            broadcast_index,
+            rx_source_index,
+        }
+    }
+}
+
+impl<'a> TryFrom<&'a str> for Circuit<'a> {
+    type Error = ParseInputError<'a>;
+
+    fn try_from(text: &'a str) -> Result<Self, Self::Error> {
+        let mut gate_builders: Vec<GateBuilder<'a>> = Vec::new();
+        let mut name_lookup = HashMap::new();
+
+        let button = GateBuilder::new("button", GateType::Button, smallvec!["broadcaster"]);
+        name_lookup.insert(button.name, 0);
+        gate_builders.push(button);
+
+        for line in text.lines() {
+            let builder: GateBuilder<'a> = line.try_into()?; // TryFrom<str> for GateBuilder
+            let index = gate_builders.len();
+            name_lookup.insert(builder.name, index);
+            gate_builders.push(builder);
+        }
+
+        // Make sure all destinations exist
+        for index in 0..gate_builders.len() {
+            for j in 0..gate_builders[index].destinations.len() {
+                let dest_name = gate_builders[index].destinations[j];
+                if let Entry::Vacant(v) = name_lookup.entry(dest_name) {
+                    let index = gate_builders.len();
+                    v.insert(index);
+                    gate_builders.push(GateBuilder::new(
+                        dest_name,
+                        GateType::Identity,
+                        SmallVec::new(),
+                    ));
+                }
+            }
+        }
+
+        let mut gates: Vec<Gate<'a>> = gate_builders
+            .into_iter()
+            .map(|g: GateBuilder<'a>| g.build(&name_lookup))
+            .try_collect()?;
+
+        
+        for index in 0..gates.len() {
+            for j in 0..gates[index].destinations.len() {
+                let dest_index = gates[index].destinations[j];
+                gates[dest_index].sources.push(index);
+                gates[dest_index].source_mask |= 1u64 << index;
+            }
+        }
+
+        let button_index = 0;
+        let broadcast_index = gates[button_index].destinations[0];
+        let rx_index = name_lookup.get("rx").copied();
+        let rx_source_index = try { gates[rx_index?].sources[0] };
+        Ok(Circuit::new(
+            gates,
+            button_index,
+            broadcast_index,
+            rx_source_index,
+        ))
     }
 }
 
@@ -212,65 +261,65 @@ enum ParseInputError<'a> {
     InvalidName(&'a str),
 }
 
-#[derive(Debug, Clone)]
-struct Input<'a> {
-    modules: Vec<Module<'a>>,
-    button_index: usize,
-    rx_index: Option<usize>,
+struct GateBuilder<'a> {
+    name: &'a str,
+    gate_type: GateType,
+    destinations: SmallVec<[&'a str; 6]>,
 }
 
-impl<'a> Input<'a> {
-    fn new(modules: Vec<Module<'a>>, button_index: usize, rx_index: Option<usize>) -> Self {
+impl<'a> GateBuilder<'a> {
+    pub fn new(name: &'a str, gate_type: GateType, destinations: SmallVec<[&'a str; 6]>) -> Self {
         Self {
-            modules,
-            button_index,
-            rx_index,
+            name,
+            gate_type,
+            destinations,
         }
+    }
+
+    pub fn build(
+        self,
+        name_lookup: &HashMap<&'a str, usize>,
+    ) -> Result<Gate<'a>, ParseInputError<'a>> {
+        let destinations = self
+            .destinations
+            .iter()
+            .map(|n| Ok(*name_lookup.get(n).ok_or(ParseInputError::InvalidName(n))?))
+            .collect::<Result<_, _>>()?;
+
+        Ok(Gate::new(self.name, self.gate_type, destinations))
     }
 }
 
-impl<'a> TryFrom<&'a str> for Input<'a> {
+impl<'a> TryFrom<&'a str> for GateBuilder<'a> {
     type Error = ParseInputError<'a>;
 
-    fn try_from(text: &'a str) -> Result<Self, Self::Error> {
-        let mut module_builders: Vec<ModuleBuilder<'a>> = Vec::new();
-        let mut name_lookup = HashMap::new();
-        let button = ModuleBuilder::new("button", ModuleType::Button, smallvec!["broadcaster"]);
-        name_lookup.insert(button.name, 0);
-        module_builders.push(button);
-        for line in text.lines() {
-            let builder: ModuleBuilder<'a> = line.try_into()?;
-            let index = module_builders.len();
-            name_lookup.insert(builder.name, index);
-            module_builders.push(builder);
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
+        if value.is_empty() {
+            return Err(ParseInputError::EmptyInput);
         }
-        for index in 0..module_builders.len() {
-            for j in 0..module_builders[index].destinations.len() {
-                let dest_name = module_builders[index].destinations[j];
-                if let Entry::Vacant(v) = name_lookup.entry(dest_name) {
-                    let index = module_builders.len();
-                    v.insert(index);
-                    module_builders.push(ModuleBuilder::new(
-                        dest_name,
-                        ModuleType::Sink,
-                        SmallVec::new(),
-                    ));
-                }
-            }
+
+        let (mut gate_type, rest) = if let Some(rest) = value.strip_prefix('%') {
+            (GateType::FlipFlop, rest)
+        } else if let Some(rest) = value.strip_prefix('&') {
+            (GateType::Conjunction, rest)
+        } else {
+            (GateType::Identity, value)
+        };
+
+        let (name, rest) = rest
+            .split_once(" -> ")
+            .ok_or(ParseInputError::ExpectedArrow)?;
+
+        let mut destinations = SmallVec::new();
+        for piece in rest.split(", ") {
+            destinations.push(piece);
         }
-        let mut modules: Vec<Module<'a>> = module_builders
-            .into_iter()
-            .map(|b: ModuleBuilder<'a>| b.build(&name_lookup))
-            .try_collect()?;
-        for index in 0..modules.len() {
-            for j in 0..modules[index].destinations.len() {
-                let dest_index = modules[index].destinations[j];
-                modules[dest_index].sources.push(index);
-                modules[dest_index].source_mask |= 1u64 << index;
-            }
+
+        if matches!(gate_type, GateType::Identity) && name != "broadcaster" {
+            gate_type = GateType::Identity;
         }
-        let rx_index = name_lookup.get("rx").copied();
-        Ok(Input::new(modules, 0, rx_index))
+
+        Ok(Self::new(name, gate_type, destinations))
     }
 }
 
@@ -283,18 +332,18 @@ mod tests {
 
     #[bench]
     fn run_parse_input(b: &mut Bencher) {
-        b.iter(|| black_box(Input::try_from(INPUT).expect("Parse input")));
+        b.iter(|| black_box(Circuit::try_from(INPUT).expect("Parse input")));
     }
 
     #[bench]
     fn run_part_1(b: &mut Bencher) {
-        let input = Input::try_from(INPUT).expect("Parse input");
+        let input = Circuit::try_from(INPUT).expect("Parse input");
         b.iter(|| black_box(part_1(&input)));
     }
 
     #[bench]
     fn run_part_2(b: &mut Bencher) {
-        let input = Input::try_from(INPUT).expect("Parse input");
+        let input = Circuit::try_from(INPUT).expect("Parse input");
         b.iter(|| black_box(part_2(&input)));
     }
 }
