48222c6f7b18f41be1c70afdb96ef0d0c2266643
Markus Jarderot <mizardx@gmail.com>
2024-01-01 13:38:30 +0100

    Day 05 - Optimize parsing

--- src/day05/mod.rs
+++ src/day05/mod.rs
@@ -1,7 +1,7 @@
+use bstr::ByteSlice;
+use bstr_parse::{BStrParse, ParseIntError};
 use std::cmp::Ordering;
 use std::fmt::Debug;
-use std::num::ParseIntError;
-use std::str::FromStr;
 use thiserror::Error;
 
 const EXAMPLE: &str = include_str!("example.txt");
@@ -53,11 +53,6 @@ pub fn part_1(input: &Input) -> isize {
 
 #[must_use]
 pub fn part_2(input: &Input) -> isize {
-    let mut seed_ranges = Vec::with_capacity(input.seeds.len() / 2);
-    for &[start, len] in input.seeds.array_chunks() {
-        seed_ranges.push(Range::new(start, len));
-    }
-    seed_ranges.sort_unstable();
     let mut location = 0;
     loop {
         let mut cur = location;
@@ -76,7 +71,7 @@ pub fn part_2(input: &Input) -> isize {
                 _ => (),
             }
         }
-        if seed_ranges.binary_search_by(|r| r.cmp_value(cur)).is_ok() {
+        if input.seed_ranges.binary_search_by(|r| r.cmp_value(cur)).is_ok() {
             return location;
         }
         location += min_delta;
@@ -187,20 +182,25 @@ impl Debug for Mapping {
 }
 
 fn numfmt(x: isize) -> String {
-    x.to_string()
-        .as_bytes()
-        .rchunks(3)
-        .rev()
-        .map(std::str::from_utf8)
-        .collect::<Result<Vec<&str>, _>>()
-        .unwrap()
-        .join(" ")
+    let x_str = x.abs().to_string().into_bytes();
+    let mut s = String::with_capacity((x_str.len() + 2) / 3 * 4 + 1);
+    if x < 0 {
+        s.push('-');
+    }
+    for chunk in x_str.rchunks(3).rev() {
+        if s.len() > 1 {
+            s.push(' ');
+        }
+        s.push_str(std::str::from_utf8(chunk).unwrap());
+    }
+    s
 }
 
 #[derive(Debug, Error)]
 enum ParseError {
     #[error("Input is empty")]
     EmptyInput,
+    #[cfg(debug_assertions)]
     #[error("First line should start with 'Seeds:'")]
     SeedSuffix,
     #[error("The first number, source_start, is missing.")]
@@ -216,11 +216,11 @@ enum ParseError {
     ExtraneousValues,
 }
 
-impl FromStr for Mapping {
-    type Err = ParseError;
+impl<'a> TryFrom<&'a [u8]> for Mapping {
+    type Error = ParseError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut it = s.split_ascii_whitespace();
+    fn try_from(s: &'a [u8]) -> Result<Self, Self::Error> {
+        let mut it = s.split(|&ch| ch == b' ');
         let destination_start: isize = it.next().ok_or(ParseError::MissingDestination)?.parse()?;
         let source_start: isize = it.next().ok_or(ParseError::MissingSource)?.parse()?;
         let len: isize = it.next().ok_or(ParseError::MissingLen)?.parse()?;
@@ -239,20 +239,27 @@ impl FromStr for Mapping {
 #[derive(Debug, Clone)]
 pub struct Input {
     seeds: Vec<isize>,
+    seed_ranges: Vec<Range>,
     mappings: Vec<Vec<Mapping>>,
     mappings2: Vec<Vec<Mapping>>,
 }
 
 fn parse_input(text: &str) -> Result<Input, ParseError> {
-    let mut lines = text.lines();
-    let seeds: Vec<isize> = lines
-        .next()
-        .ok_or(ParseError::EmptyInput)?
-        .strip_prefix("seeds: ")
-        .ok_or(ParseError::SeedSuffix)?
-        .split_ascii_whitespace()
-        .map(str::parse)
-        .collect::<Result<Vec<_>, _>>()?;
+    let mut lines = text.as_bytes().lines();
+    let first_line = lines.next().ok_or(ParseError::EmptyInput)?;
+    #[cfg(debug_assertions)]
+    if &first_line[..7] != b"seeds: " {
+        return Err(ParseError::SeedSuffix);
+    }
+    let mut seeds = Vec::with_capacity(20);
+    for num_str in first_line[7..].split(|&ch| ch == b' ') {
+        seeds.push(num_str.parse()?);
+    }
+    let mut seed_ranges = Vec::with_capacity(seeds.len()/2);
+    for &[a,b] in seeds.array_chunks() {
+        seed_ranges.push(Range::new(a,b));
+    }
+    seed_ranges.sort_unstable();
 
     let mut mappings = Vec::with_capacity(10);
     let mut current: Vec<Mapping> = Vec::with_capacity(50);
@@ -273,7 +280,7 @@ fn parse_input(text: &str) -> Result<Input, ParseError> {
                 mappings.push(tmp);
             }
         } else {
-            current.push(line.parse()?);
+            current.push(line.try_into()?);
         }
     }
     current.sort_unstable();
@@ -286,6 +293,7 @@ fn parse_input(text: &str) -> Result<Input, ParseError> {
 
     Ok(Input {
         seeds,
+        seed_ranges,
         mappings,
         mappings2,
     })
